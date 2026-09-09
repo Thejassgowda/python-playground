36edd2ccf13a67b9eef1d5ae25ac831ecf6f55ff
Thejas S Gowda <mgthejas89@gmail.com>
2026-09-09 20:16:13 +0530

    Refactor problem.py for clarity and functionality

--- problem.py
+++ problem.py
@@ -1,8 +1,9 @@
+# take input as name and age from the user and print
 name=input("enter your name:")
 age=int(input("enter your age:"))
 print('Hi',name , "next year youll be " , age+1)
 
-
+# simle calculater
 n1=float(input("enter a 1st number:"))
 n2=float(input("enter a 2st number:"))
 sum=n1+n2
@@ -13,13 +14,15 @@ print("sum is :",sum ,
       "diff is:" ,diff ,
       "pro is :" ,pro ,
       "qou is :",quo)
-      
+
+# print firstname and last naame
 fullname=input("enter full name:")
 parts=fullname.split()
 print("first name",parts[0])
 print("last name",parts[-1])
 
-  
+
+#count number of vowels in the entered sentence
 sent = input("enter a word: ")
 vowels ="aeiouAEIOU"
 count = 0
@@ -27,7 +30,7 @@ for ch in sent :
     if ch in vowels:
         count=count+1
         print(count)
-        
+    #check the number is  palandrom    
 num=input("enter a number:")
 if num==num[::-1]:
     print('palandrom',num)
